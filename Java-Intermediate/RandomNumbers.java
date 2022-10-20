c4795864e7e132c71f4ae289a1f19a8d6ee7a266
K0dikas <kodikasYT@protonmail.com>
2022-10-20 17:22:46 +0800

    Added how to generate random numbers

---
+++ Java-Intermediate/RandomNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class RandomNumbers{
+
+	public static void main(String[] args){
+
+		Random generate = new Random();
+		int x = generate.nextInt(6); // It will generate numbers from 0 to 5, because computer always start at 0
+									 // To solve it, just do it like this; (6)+1;
+		System.out.println(x);
+
+		double decimal = generate.nextDouble();
+		System.out.println(decimal);
+	
+		boolean crap = generate.nextBoolean();
+		System.out.println(crap);
+	}
+}
