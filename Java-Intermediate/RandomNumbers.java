58c54c4abd84681f6b13ed334d73eae1375f5afd
K0dikas <kodikasYT@protonmail.com>
2022-10-20 17:24:41 +0800

    fixed one comment, indentation

--- Java-Intermediate/RandomNumbers.java
+++ Java-Intermediate/RandomNumbers.java
@@ -6,7 +6,7 @@ public class RandomNumbers{
 
 		Random generate = new Random();
 		int x = generate.nextInt(6); // It will generate numbers from 0 to 5, because computer always start at 0
-									 // To solve it, just do it like this; (6)+1;
+						// To solve it, just do it like this; (6)+1;
 		System.out.println(x);
 
 		double decimal = generate.nextDouble();
