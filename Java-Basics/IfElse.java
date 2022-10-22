e4ec09eba722cc12ea5825b0b8a28168955e8054
K0dikas <kodikasYT@protonmail.com>
2022-10-22 17:38:34 +0800

    modified one name, Brian to B...n

--- Java-Basics/IfElse.java
+++ Java-Basics/IfElse.java
@@ -33,7 +33,7 @@ public class IfElse{
 			}
 
 			else if (num == 3){
-				System.out.println("Someone is having a crush on Brian");
+				System.out.println("Someone is having a crush on B...n");
 			}
 
 			else if (num == 4){
